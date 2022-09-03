2f6a9aff887d54a7c2a92beb3a75c367301cc4fd
Jacobo Rabadan <jacobortgtrader@gmail.com>
2022-09-03 18:39:05 -0500

    Primer commit

---
+++ JS/menuScroll.js
@@ -0,0 +1,5 @@
+//Menu con efecto scroll
+window.addEventListener('scroll' , function() {
+    let header = document.querySelector('header');
+    header.classList.toggle('abajo', window.scrollY>0);
+})
